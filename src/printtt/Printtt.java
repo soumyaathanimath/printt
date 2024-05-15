7cc05e15cce70baf2568c0961500fe095d7fb599
soumyaathanimath <>
2024-05-15 14:45:54 +0530

    printing

---
+++ src/printtt/Printtt.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printtt;
+
+/**
+ *
+ * @author dell1
+ */
+public class Printtt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          /*int a=10;
+        float b=12.55f;
+        char c='A';
+        String str="Hello";
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(str);*/
+        
+        
+        int x=10,y=20;
+        System.out.println(x+y);
+        System.out.println("Number is " +y);
+        System.out.println(x+y+" sum");
+        System.out.println("sum "+(x+y));
+        System.out.println("sum of "+x+" and "+y+" is "+(x+y));
+    }
+    
+}
